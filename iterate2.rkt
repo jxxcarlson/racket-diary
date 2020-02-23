cf694d71ffa0509e89bcae7b1b9903bca6687199
James Carlson <jxxcarlson@mac.com>
2020-02-23 10:15:07 +0100

    Add file

---
+++ iterate2.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require 2htdp/image)
+(require 2htdp/universe)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Iteration: functions of two variables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (iterate2 f n step a b)
+  (cond
+   [(<= n 0) b]
+   [else (iterate2 f (- n 1) step (+ step a) (f (+ step a) b))]
+   ))
+
+
+;; Image functions
+
+(define (place-circle x y image)
+  (place-image (circle 5 "solid" "green")
+               x y image))
+
+(define CANVAS (empty-scene 300 600))
+
+(define (fc t image)
+  (place-circle (* 3 t) (* 0.1 t t) image))
+
+;; Example
+
+(iterate2 fc 15 5 0 CANVAS)
