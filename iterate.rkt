d9e388a2ba33ca02c86c1c4202315ba26352d432
James Carlson <jxxcarlson@mac.com>
2020-02-23 13:15:05 +0100

    Define function to test iterations of crazy

--- iterate.rkt
+++ iterate.rkt
@@ -6,11 +6,13 @@
 
 ;; https://docs.racket-lang.org/guide/Lists__Iteration__and_Recursion.html#%28part._tail-recursion%29
 
+
 (define (iterate f n a)
-  (cond
-   [(= n 0) a]
-   [else (iterate f (- n 1) (f a))]
-   ))
+  (if (= n 0)
+      a
+      (iterate f (- n 1) (f a))
+      )
+  )
 
 (define (inc x) (+ x 1))
 (define (double x) (* 2 x))
@@ -20,7 +22,7 @@
       (quotient n 2)
       (+ 1 (* 3 n))))
 
-
+(define (f n a) (iterate crazy n a))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Examples
@@ -29,4 +31,6 @@
 ;; (iterate double 3 1) --> 8
 ;; (crazy 4)
 ;; (crazy 5)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;       
+;; (f 5 33)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  
